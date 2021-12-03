c11e87f06acca0ed26fa9a698dc648651b539150
jyw <714348906@qq.com>
2021-12-03 16:01:56 +0800

    删除不该提交的东西；

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,27 +2,18 @@
   <div>
     首页,会需要一个统计表,
     等我思考下
-    {{ JSON.stringify(selectValue) }}
   </div>
-  <TheSelect 
-    :options="[{name: '选项1', code: 1}, { name: '选项2', code: 2 }]" 
-    value-key="code"
-    label-key="name"
-    v-model="selectValue"
-  />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import TheSelect from "@/components/TheSelect/index.vue"
+import { defineComponent } from 'vue'
 
 
 export default defineComponent({
-  components: { TheSelect },
   setup() {
 
     return {
-      selectValue: ref([])
+      
     }
   }
 })
